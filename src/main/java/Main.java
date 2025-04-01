b8ac9ed86fc6fcd24aa7d1c498b0864ac621950d
Patrick <proconn@icloud.com>
2025-04-01 14:42:01 +0100

    Ran some tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,44 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
+
+import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
         SortingAlgorithm countSort = SortingAlgorithms::countingSort;
         SortingAlgorithm ARUCountSort = SortingAlgorithms::ARUCountingSort;
 
-        long countSortTime;
-        long ARUCountSortTime;
-        for(int i = 0; i < 100; i++){
-            countSortTime = benchmarkSort(countSort, 100000000, 100000000);
-            ARUCountSortTime = benchmarkSort(ARUCountSort, 100000000,100000000);
-            System.out.println("Time :\tCount :\t"+countSortTime/1e9+"\tARU Count :\t"+ARUCountSortTime/1e9+"\tDelta : \t"+ (countSortTime - ARUCountSortTime)/1e9 + "\t" + (((double)countSortTime/(double)ARUCountSortTime)*100 - 100) + "%");
+        FileWriter outputFile;
+        try {outputFile = new FileWriter("sortingTimes2.csv");}catch (Exception e){
+            throw new RuntimeException("File failed to open");
         }
 
+        long countSortTime;
+        long ARUCountSortTime;
+        int n;
+        for (int exp = 1; exp <= 100; exp++){
+            for (int i = 0; i < 10; i++) {
+                n = (int)1e5 * exp;
+                countSortTime = benchmarkSort(countSort, n, n);
+                ARUCountSortTime = benchmarkSort(ARUCountSort, n, n);
+                System.out.println("Time :\tCount :\t" + countSortTime / 1e9 + "\tARU Count :\t" + ARUCountSortTime / 1e9 + "\tDelta : \t" + (countSortTime - ARUCountSortTime) / 1e9 + "\t" + (((double) countSortTime / (double) ARUCountSortTime) * 100 - 100) + "%");
 
+                try {
+                    outputFile.write((int)pow(2,exp)+","+countSortTime/ 1e9 + "," + ARUCountSortTime/ 1e9 + "\n");
+                } catch (IOException e) {
+                    throw new RuntimeException("File failed to be written to");
+                }
+            }
+            System.out.println(exp);
+        }
+
+        try {
+            outputFile.close();
+        } catch (IOException e) {
+            throw new RuntimeException("File failed to close");
+        }
+        System.out.println("Test finished");
     }
 
     public static long benchmarkSort(SortingAlgorithm sort, int l, int k){
