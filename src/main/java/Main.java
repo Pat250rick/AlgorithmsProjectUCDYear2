8714e33a83239511ea7cf43668acbb025f6d5756
Patrick <proconn@icloud.com>
2025-04-01 11:18:08 +0100

    Implemented Sorting Algorithms

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SortingAlgorithm countSort = SortingAlgorithms::countingSort;
+        SortingAlgorithm ARUCountSort = SortingAlgorithms::ARUCountingSort;
+
+        long countSortTime;
+        long ARUCountSortTime;
+        for(int i = 0; i < 100; i++){
+            countSortTime = benchmarkSort(countSort, 100000000, 100000000);
+            ARUCountSortTime = benchmarkSort(ARUCountSort, 100000000,100000000);
+            System.out.println("Time :\tCount :\t"+countSortTime/1e9+"\tARU Count :\t"+ARUCountSortTime/1e9+"\tDelta : \t"+ (countSortTime - ARUCountSortTime)/1e9 + "\t" + (((double)countSortTime/(double)ARUCountSortTime)*100 - 100) + "%");
+        }
+
+
+    }
+
+    public static long benchmarkSort(SortingAlgorithm sort, int l, int k){
+
+        Random rand = new Random();
+        int [] test = new int[l];
+        int max = 0;
+        for(int i = 0; i < l; i++){
+            test[i] = rand.nextInt(k);
+            if (test[i] > max){
+                max = test[i];
+            }
+        }
+
+        long start = System.nanoTime();
+        sort.sort(test,max);
+        return System.nanoTime() - start;
+
     }
 }
---
+++ src/main/java/SortingAlgorithms.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.sqrt;
+
+
+public final class SortingAlgorithms {
+    public static int [] countingSort(int [] a, int k){
+        int n = a.length;
+
+        int [] b = new int [n];
+        int [] c = new int [k + 1];
+
+        int i = 0;
+        int j = 0;
+
+        while ( i < k+1 ) {
+            c[i] = 0;
+            i++;
+        }
+
+        while ( j < n )
+        {
+            c[a[j]]++;
+            j++;
+        }
+
+        i = 1;
+        j = n-1;
+
+        while ( i <= k)
+        {
+            c[i] = c[i] + c[i-1];
+            i++;
+        }
+
+        while ( j >= 0)
+        {
+            b[c[a[j]] - 1] = a[j];
+            c[a[j]]--;
+            j--;
+        }
+
+        return b;
+    }
+
+    public static int [] ARUCountingSort(int [] a, int k){
+        int n = a.length;
+        int m = (int)ceil(sqrt(k));
+
+        int [] b = new int[n];
+        int [] q = new int[m+1];    // Represents the quotient array
+        int [] r = new int[m+1];    // Represents the remainder array
+
+        int i = 1;
+        int j = 0;
+
+        while ( j < n )
+        {
+            q[a[j]/m]++;
+            r[a[j]%m]++;
+            j++;
+        }
+
+        while ( i <= m )
+        {
+            q[i] = q[i] + q[i-1];
+            r[i] = r[i] + r[i-1];
+            i++;
+        }
+
+        j = n-1;
+        int d;
+
+        while ( j >= 0 )
+        {
+            d = a[j]%m;
+            r[d]--;
+            b[r[d]] = a[j];
+            j--;
+        }
+
+        i = n-1;
+
+        while ( i >= 0 )
+        {
+            d = b[i]/m;
+            q[d]--;
+            a[q[d]] = b[i];
+            i = i - 1;
+        }
+
+        return a;
+    }
+
+}
